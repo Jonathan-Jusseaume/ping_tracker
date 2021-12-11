98ba7d9050552659836c4d6a29e0100668156ba1
JotaroE21 <jusseaumejonathan5@gmail.com>
2021-12-11 11:33:18 +0100

    Mise en place de la connexion à la base de données et inspectdb.

--- ping_tracker/settings.py
+++ ping_tracker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ping_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ping-tracker',
+        'USER': 'user',
+        'PASSWORD': 'testing-password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
